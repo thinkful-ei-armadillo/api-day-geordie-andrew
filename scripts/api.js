4caf8374aefc92ae7ea25c5f0a4615975bdd1ded
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-28 09:28:02 -0800

    exercises up to number 6

---
+++ scripts/api.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const api = (function(){
+	const BASE_URL = 'https://thinkful-list-api.herokuapp.com/andrew';
+	function getItems(){
+		return fetch(`${BASE_URL}/items`);
+	}
+    
+	function createItem(name){
+		const newItem = JSON.stringify({
+			name
+		});
+
+		const options = {
+			method: 'POST',
+			headers: new Headers({
+				'Content-Type': 'application/json'
+			}),
+			body: newItem
+		};
+        
+
+        
+		return fetch(`${BASE_URL}/items`, options);
+	}
+
+	return {
+		getItems,
+		createItem
+	};
+}());
